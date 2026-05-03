0b090eb8a61b9b9e91334b490363675298091f9d
jonasmgs <gastroon@gmail.com>
2026-05-03 11:34:04 -0300

    chore: increase vite build chunk size warning limit to 700kb

--- vite.config.ts
+++ vite.config.ts
@@ -75,6 +75,7 @@ export default defineConfig(({ mode }) => ({
     include: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime"],
   },
   build: {
+    chunkSizeWarningLimit: 700,
     rollupOptions: {
       output: {
         manualChunks: {
